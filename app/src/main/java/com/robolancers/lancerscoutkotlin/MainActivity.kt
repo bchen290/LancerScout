e404687c38169396b315a2d9f172ef3cc1e9dcf1
Brian Chen <brianchen0810@hotmail.com>
2020-03-14 16:10:02 -0400

    Add initial template activity

--- app/src/main/java/com/robolancers/lancerscoutkotlin/MainActivity.kt
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.robolancers.lancerscoutkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.robolancers.lancerscoutkotlin.activities.TemplateActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val templateButton = findViewById<Button>(R.id.template_button)
+        templateButton.setOnClickListener {
+            startActivity(Intent(this, TemplateActivity::class.java))
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/activities/TemplateActivity.kt
@@ -0,0 +1,12 @@
+package com.robolancers.lancerscoutkotlin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.robolancers.lancerscoutkotlin.R
+
+class TemplateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_template)
+    }
+}
