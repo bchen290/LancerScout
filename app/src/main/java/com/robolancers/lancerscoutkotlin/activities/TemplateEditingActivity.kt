d840f39ac58dc962ba830338e05c20d31ea251a2
Brian Chen <brianchen0810@hotmail.com>
2020-03-15 15:47:27 -0400

    Add Initial TemplateEditingActivity

---
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/activities/TemplateEditingActivity.kt
@@ -0,0 +1,13 @@
+package com.robolancers.lancerscoutkotlin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.robolancers.lancerscoutkotlin.R
+
+class TemplateEditingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_template_editing)
+    }
+}
