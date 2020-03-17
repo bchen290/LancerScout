aa938adabb1991e4b5dd3c1c9f500eeaf9809fea
Brian Chen <brianchen0810@hotmail.com>
2020-03-17 13:19:45 -0400

    Add toolbar to TemplateEditingActivity

--- app/src/main/java/com/robolancers/lancerscoutkotlin/activities/TemplateEditingActivity.kt
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/activities/TemplateEditingActivity.kt
@@ -1,7 +1,7 @@
 package com.robolancers.lancerscoutkotlin.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,11 +12,15 @@ import com.robolancers.lancerscoutkotlin.models.template.TemplateModel
 import com.robolancers.lancerscoutkotlin.utilities.ItemTouchHelperSimpleCallback
 import com.robolancers.lancerscoutkotlin.utilities.LancerDialogFragment
 import com.robolancers.lancerscoutkotlin.utilities.TemplateEditingAdapter
+import com.robolancers.lancerscoutkotlin.utilities.ToolbarActivity
 
-class TemplateEditingActivity : AppCompatActivity(), LancerDialogFragment.LancerDialogListener {
+class TemplateEditingActivity : ToolbarActivity(), LancerDialogFragment.LancerDialogListener {
     private lateinit var templateEditingRecyclerView: RecyclerView
     private lateinit var templateEditingAdapter: TemplateEditingAdapter<TemplateModel>
     private val templateEditingList = mutableListOf<TemplateModel>()
+
+    private lateinit var templateType: String
+    private lateinit var templateName: String
 
     private val templateEditingHelper by lazy {
         ItemTouchHelper(ItemTouchHelperSimpleCallback(applicationContext, templateEditingAdapter).simpleItemCallback)
@@ -25,6 +29,14 @@ class TemplateEditingActivity : AppCompatActivity(), LancerDialogFragment.Lancer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_template_editing)
+
+        setupToolbar()
+
+        templateType = intent.getStringExtra("Type")
+        templateName = intent.getStringExtra("ItemClicked")
+
+        val templateEditingTitle = findViewById<TextView>(R.id.template_editing_title)
+        templateEditingTitle.text = templateName
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
