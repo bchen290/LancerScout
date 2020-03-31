cce25c488042816f2f1e5695a3a662783feb7e52
Brian Chen <brianchen0810@hotmail.com>
2020-03-31 14:01:12 -0400

    Add id to entities to allow duplicate names

--- app/src/main/java/com/robolancers/lancerscoutkotlin/room/PitTemplate.kt
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/room/PitTemplate.kt
@@ -4,4 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "pit_templates")
-data class PitTemplate(@PrimaryKey var name: String, var data: String)
+data class PitTemplate(var name: String, var data: String) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
