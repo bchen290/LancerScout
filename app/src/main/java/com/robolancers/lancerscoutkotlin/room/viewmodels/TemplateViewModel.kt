83f99b1d2ead5911f4443dcc979183e372fe0312
Brian Chen <brianchen0810@hotmail.com>
2020-03-31 21:28:41 -0400

    Add more padding to list

--- app/src/main/java/com/robolancers/lancerscoutkotlin/room/viewmodels/TemplateViewModel.kt
+++ app/src/main/java/com/robolancers/lancerscoutkotlin/room/viewmodels/TemplateViewModel.kt
@@ -1,7 +1,6 @@
 package com.robolancers.lancerscoutkotlin.room.viewmodels
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
